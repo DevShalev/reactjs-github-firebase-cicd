e378a5c2db4fafe3d584e400414e913d90f46eb6
DevShalev <94471389+DevShalev@users.noreply.github.com>
2023-08-31 20:15:23 +0300

    update message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from CI/CD
+          מורן אני אוהב אותך מלאנתאלפים גאהרם!
         </p>
         <a
           className="App-link"
